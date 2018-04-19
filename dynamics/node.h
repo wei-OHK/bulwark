913cdf5c8ab186d7ded6ac5b081ec424d4a3118b
wei-ForPrivateUse <931995113@qq.com>
2018-04-19 22:17:28 +0900

    v1.3
    Added Node::GetFlag
    Added Node::SetFlag
    Added Node::AddFlag
    Added Node::RemoveFlag
    Added Node::CheckFlag
    Added Node::GetUserData
    Added Node::SetUserData

--- dynamics/node.h
+++ dynamics/node.h
@@ -31,13 +31,17 @@ public:
 
 		std::size_t Id = 0;
 		unsigned int Tag = 0;
+		
+		unsigned int Flag = 0x0;
+		void* UserData = nullptr;
 
 		Node_Type const NodeType;
 		manager::SceneMgr* SceneManager = nullptr;
 	};
 
 	Node(Configuration* __conf) : _M_type(__conf->NodeType), _M_id(__conf->Id),
-			_M_tag(__conf->Tag), _M_scenemgr(__conf->SceneManager) { }
+			_M_tag(__conf->Tag), _M_flag(__conf->Flag), _M_user_data(__conf->UserData), 
+					_M_scenemgr(__conf->SceneManager) { }
 	virtual ~Node() { }
 
 	/// Getters.
@@ -53,6 +57,38 @@ public:
 		return _M_tag;
 	}
 
+	unsigned int GetFlag() const {
+		return _M_flag;
+	}
+
+	void SetFlag(unsigned int __flag) {
+		_M_flag = __flag;
+	}
+
+	void AddFlag(unsigned int __flag) {
+		_M_flag = _M_flag | __flag;
+	}
+
+	void RemoveFlag(unsigned int __flag) {
+		_M_flag = _M_flag & (!__flag);
+	}
+
+	bool CheckFlag(unsigned int __flag) const {
+		return _M_flag&__flag == __flag;
+	}
+
+	void* GetUserData() {
+		return _M_user_data;
+	}
+
+	const void* GetUserData() const {
+		return _M_user_data;
+	}
+
+	void SetUserData(void* __user_data) {
+		_M_user_data = __user_data;
+	}
+
 	manager::SceneMgr* GetSceneMgr() {
 		return _M_scenemgr;
 	}
@@ -66,6 +102,9 @@ private:
 
 	std::size_t const _M_id;
 	unsigned int const _M_tag;
+
+	unsigned int _M_flag;
+	void* _M_user_data;
 
 	manager::SceneMgr* const _M_scenemgr;
 };
